1d407573ad0dc852bd2f7055c47cf093b4d861be
hamid <hamidghahremani2001@gmail.com>
2026-05-22 10:55:31 +0330

    setup basic project structure

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	app := gin.Default()
+
+	app.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "hello world!"})
+	})
+
+	app.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "pong"})
+	})
+
+	app.Run("127.0.0.1:8000")
 }
