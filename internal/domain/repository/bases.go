f8d4fade28db14fa5bc5a06c14fd57bad85efc26
hamid <hamidghahremani2001@gmail.com>
2026-05-29 16:17:13 +0330

    add User Repository Interface (+ base generic repositories)

---
+++ internal/domain/repository/bases.go
@@ -0,0 +1,24 @@
+package repository
+
+import (
+	"context"
+
+	e "Go-Blog-API/internal/domain/entity"
+)
+
+type TDBEntities interface {
+	e.User | e.FollowsM2M | e.Link | e.Post | e.PostLikesM2M | e.Comment |
+		e.Tag | e.PostTagsM2M | e.List | e.SavedPostsM2M | e.UsersSavedListsM2M
+}
+
+type creator[TEntity TDBEntities] interface {
+	Create(ctx context.Context, entity *TEntity) (*TEntity, error)
+}
+
+type deleter interface {
+	Delete(ctx context.Context, pk uint64) error
+}
+
+type getterByID[TEntity TDBEntities] interface {
+	GetByID(ctx context.Context, pk uint64) (*TEntity, error)
+}
--- internal/domain/repository/repository.go
+++ internal/domain/repository/repository.go
@@ -1,1 +1,20 @@
 package repository
+
+import (
+	"context"
+
+	"Go-Blog-API/internal/domain/entity"
+)
+
+type UserRepository interface {
+	creator[entity.User]
+	UpdateUsername(ctx context.Context, pk uint64, username string) (*entity.User, error)
+	UpdateEmail(ctx context.Context, pk uint64, email string) (*entity.User, error)
+	UpdateBio(ctx context.Context, pk uint64, bio string) (*entity.User, error)
+	UpdatePassword(ctx context.Context, pk uint64, password string) (*entity.User, error)
+	UpdateEnabled(ctx context.Context, pk uint64, enabled bool) (*entity.User, error)
+	deleter
+	getterByID[entity.User]
+	GetByUsername(ctx context.Context, username string) (*entity.User, error)
+	GetByEmail(ctx context.Context, email string) (*entity.User, error)
+}
